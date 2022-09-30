fd694a9e4b9118fcb7ededee27f08cd37ee26314
PaolaCasadiegosVaca <paola.casadiegos@valid.com>
2022-09-29 19:06:29 -0500

    IMPLEMENTACION: Se implementa busqueda de usuario por id.

--- BackEnd/src/main/java/com/example/demo/controller/UserController.java
+++ BackEnd/src/main/java/com/example/demo/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,6 +45,14 @@ public class UserController {
 		}
 			
 		
+		return new ResponseEntity<>(mensage,null,HttpStatus.OK);
+	}
+	@GetMapping("/login/{id}")
+	public ResponseEntity<Map<String,Object>> getUserById(@PathVariable Integer id){
+		UserModel user = new UserModel();
+		user= Servicio.UserId(id);
+		Map<String,Object> mensage = new HashMap<>();
+		mensage.put("User", user);
 		return new ResponseEntity<>(mensage,null,HttpStatus.OK);
 	}
 
